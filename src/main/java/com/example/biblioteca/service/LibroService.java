8a68923a7f78f95af5f4d2dea1ae361f3395a354
Jose Luis <jos.medinam@duocuc.cl>
2025-05-09 02:22:12 -0400

    Agregaso Model,service,model y controller

---
+++ src/main/java/com/example/biblioteca/service/LibroService.java
@@ -0,0 +1,36 @@
+package com.example.biblioteca.service;
+
+import com.example.biblioteca.model.Libro;
+import com.example.biblioteca.repository.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class LibroService {
+
+    @Autowired
+    private LibroService libroRepository;
+
+    public List<Libro> getLibros() {
+        return libroRepository.obtenerLibros();
+    }
+
+    public Libro saveLibro(Libro libro) {
+        return libroRepository.guardar(libro);
+    }
+
+    public Libro getLibroId(int id) {
+        return libroRepository.buscarPorId(id);
+    }
+
+    public Libro updateLibro(Libro libro) {
+        return libroRepository.actualizar(libro);
+    }
+
+    public String deleteLibro(int id) {
+        libroRepository.eliminar(id);
+        return "Libro eliminado con exito";
+    }
+
+}
